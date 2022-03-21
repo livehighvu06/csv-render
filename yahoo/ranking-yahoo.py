29bd498b236cc8fc8a65876720584a3fea9e3515
Eddie Chen <livehighvu06@gmail.com>
2022-03-21 15:20:20 +0800

    '限制PC渲染五次 , 調整SP Template'

--- yahoo/ranking-yahoo.py
+++ yahoo/ranking-yahoo.py
@@ -1,12 +1,12 @@
 import csv
 # 重量
-weight = '3kg'
+weight = '300g'
+date = '3/11 ~ 3/20'
 file = open(f'{weight}.csv', encoding='UTF-8')
-
-
 
 test = []
 result = []
+resultSP = []
 reader = csv.reader(file)
 data_list = list(reader)
 file.close()
@@ -18,17 +18,14 @@ for data in data_list:
 # print(test)
 for i in range(0,len(test),4):
     result.append(test[i:i+4])
+
+for i in range(0,len(test),8):
+    resultSP.append(test[i:i+8])
 # print(result)
 
-htmlPC , htmlSP = ''
-templatePC = f"""
-<b style="font-size: 18px;">ホエイプロテイン売れ筋ランキング (集計期間 : 3/1～3/10)</b>
-<div style="border:#b9b9b9;padding: 20px;width: 100%;">
-<ul style="list-style:none;display: flex;padding: 0;">
-    {htmlPC}
-</ul>
-</div>
-"""
+htmlPC = ''
+htmlSP = ''
+
 index = 0
 for i in result:
     flavor = i[0]
@@ -48,22 +45,8 @@ for i in result:
         </a>
         </li>
     """
-
-
-    htmlSP+=f"""
-    <td width="48%">
-            <a href="{url}">
-                <img src="https://image.rakuten.co.jp/x-plosion/cabinet/category_ranking/r{index +1}.jpg" width="100%" />
-                <img src="{image}" width="100%" />
-                <font color="#000">
-                    プロテイン<br>{flavor}<br>{weight}<br>
-                    価格 價格<br><br>
-                </font>
-                <img src="https://image.rakuten.co.jp/x-plosion/cabinet/202006_rank_btn.jpg" width="100%" />
-            </a>
-        </td>
-    """
-    templatePC = f"""<b style="font-size: 18px;">ホエイプロテイン売れ筋ランキング (集計期間 : 3/1～3/10)</b>
+  
+    templatePC = f"""<b style="font-size: 18px;">ホエイプロテイン売れ筋ランキング (集計期間 : {date})</b>
     <div style="border:#b9b9b9;padding: 20px;width: 100%;">
     <ul style="list-style:none;display: flex;padding: 0;">
             {htmlPC}
@@ -71,10 +54,61 @@ for i in result:
     """
     index+=1
 
+
+
+indexSP = 0
+for i in resultSP:
+    flavor = i[0]
+    url =i[1]
+    image =i[2]
+    price = i[3]
+
+    flavor2 = i[4]
+    url2 =i[5]
+    image2 =i[6]
+    price2 = i[7]
+
+    htmlSP+=f"""
+        <tr>
+            <td colspan="3" height="15"></td>
+        </tr>
+        <tr>
+            <td width="48%">
+                <a href="{url}">
+                    <img src="https://shopping.c.yimg.jp/lib/x-plosion/r{indexSP +1}.jpg" width="100%" />
+                    <img src="{image}" width="100%" />
+                    プロテイン<br>{flavor}<br>{weight}<br>
+                    価格 {price}<br><br>
+                    <img src="https://shopping.c.yimg.jp/lib/x-plosion/202006_rank_btn.jpg" width="100%" />
+                </a>
+            </td>
+            <td width="4%"></td>
+             <td width="48%">
+                <a href="{url2}">
+                    <img src="https://shopping.c.yimg.jp/lib/x-plosion/r{indexSP +2}.jpg" width="100%" />
+                    <img src="{image2}" width="100%" />
+                    プロテイン<br>{flavor2}<br>{weight}<br>
+                    価格 {price2}<br><br>
+                    <img src="https://shopping.c.yimg.jp/lib/x-plosion/202006_rank_btn.jpg" width="100%" />
+                </a>
+            </td>
+        </tr>
+    """
+
+    templateSP = f"""<h1>ホエイプロテイン売れ筋ランキング</h1>
+    <h1>集計期間 : {date}</h1>
+    <table width="100%" cellpadding="0" cellspacing="0">
+    {htmlSP}
+    </table>
+    """
+
+    indexSP+=2
+
+
+
+
 with open(f'yahoo-{weight}-pc.html','w',encoding='UTF-8') as file:
     file.write(templatePC)
-with open(f'yahoo-{weight}-sp.html','w',encoding='UTF-8') as file:
-    file.write(htmlSP)
-    
-print(templatePC)
 
+with open(f'yahoo-{weight}-sp.html','w',encoding='UTF-8') as file:
+    file.write(templateSP)
--- 楽天/ranking-rakuten.py
+++ 楽天/ranking-rakuten.py
@@ -2,10 +2,12 @@ import csv
 
 ##############  請先輸入   ##############
 weight = '3kg'
+date ='3/1～3/10'
 
 file = open(f'{weight}.csv',encoding='UTF-8')
 test = []
 result = []
+resultSP = []
 reader = csv.reader(file)
 data_list = list(reader)
 file.close()
@@ -18,12 +20,20 @@ for data in data_list:
 for i in range(0,len(test),4):
     result.append(test[i:i+4])
 
+for i in range(0,len(test),8):
+    resultSP.append(test[i:i+8])
+
 htmlPC = ''
 htmlSP= ''
 
 
+# PC
 index = 0
-for i in result:
+for (index , i) in enumerate(result):
+    print(index)
+    if index>4:
+        break
+    
     flavor = i[0]
     url =i[1]
     image =i[2]
@@ -42,44 +52,70 @@ for i in result:
         </li>
     """
 
-    htmlSP+=f"""
-        <td width="48%">
-                <a href="{url}">
-                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/category_ranking/r{index +1}.jpg" width="100%" />
-                    <img src="{image}" width="100%" />
-                    <font color="#000">
-                        プロテイン<br>{flavor}<br>{weight}<br>
-                        価格 價格<br><br>
-                    </font>
-                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/202006_rank_btn.jpg" width="100%" />
-                </a>
-            </td>
-    """
-
-
-    templatePC = f"""<b style="font-size: 18px;">ホエイプロテイン売れ筋ランキング (集計期間 : 3/1～3/10)</b>
+    templatePC = f"""<b style="font-size: 18px;">ホエイプロテイン売れ筋ランキング (集計期間 : {date})</b>
     <div style="border:#b9b9b9;padding: 20px;width: 800px;">
     <ul style="list-style:none;display: flex;padding: 0;">
             {htmlPC}
     </ul></div>
     """
-
-    # templateSP = f"""
-    # <table width="100%" cellpadding="0" cellspacing="0">
-    #     <font color="#000" size="3">ホエイプロテイン売れ筋ランキング</font><br>
-    #     <font color="#000" size="3">集計期間 : 3/1～3/10</font>
-    #     <tr>
-    #         <td colspan="3" height="15"></td>
-    #     </tr>
-    #     {htmlSP}
-    # </table>
-    # """
     index+=1
+
+# SP
+for i in resultSP:
+    flavor = i[0]
+    url =i[1]
+    image =i[2]
+    price = i[3]
+
+    flavor2 = i[4]
+    url2 =i[5]
+    image2 =i[6]
+    price2 = i[7]
+
+    htmlSP+=f"""
+        <tr>
+            <td colspan="3" height="15"></td>
+        </tr>
+        <tr>
+            <td width="48%">
+                <a href="{url}">
+                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/category_ranking/r{index +1}.jpg" width="100%" />
+                    <img src="{image}" width="100%" />
+                    <font color="#000">
+                        プロテイン<br>{flavor}<br>{weight}<br>
+                        価格 {price}<br><br>
+                    </font>
+                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/202006_rank_btn.jpg" width="100%" />
+                </a>
+            </td>
+            <td width="4%"></td>
+            <td width="48%">
+                <a href="{url2}">
+                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/category_ranking/r{index +2}.jpg" width="100%" />
+                    <img src="{image2}" width="100%" />
+                    <font color="#000">
+                        プロテイン<br>{flavor2}<br>{weight}<br>
+                        価格 {price2}<br><br>
+                    </font>
+                    <img src="https://image.rakuten.co.jp/x-plosion/cabinet/202006_rank_btn.jpg" width="100%" />
+                </a>
+            </td>
+        </tr>
+    """
+
+    templateSP = f"""<table width="100%" cellpadding="0" cellspacing="0">
+    <font color="#000" size="3">ホエイプロテイン売れ筋ランキング</font><br>
+    <font color="#000" size="3">集計期間 : {date}</font>
+        {htmlSP}
+    </table>
+    """
+
+    index+=2
 
 with open(f'rakuten-{weight}-pc.html','w',encoding='UTF-8') as file:
     file.write(templatePC)
 
 with open(f'rakuten-{weight}-sp.html','w',encoding='UTF-8') as file:
-    file.write(htmlSP)
+    file.write(templateSP)
 
 
